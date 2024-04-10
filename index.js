3733ef3ef46b3f6a38619ca46bdc56b0f7abd108
Vlads21ter <shkliarskyi_ak22@nuwm.edu.ua>
2024-04-10 14:07:45 +0300

    Reworked authentication

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import {MongoClient, ServerApiVersion} from "mongodb";
 import {google} from "googleapis";
-import {Storage} from "@google-cloud/storage";
-// import { authenticate } from "@google-cloud/local-auth";
 import fs from "fs/promises";
 import path from "path";
 import process from "process";
@@ -12,7 +10,7 @@ const calendar = google.calendar('v3');
 
 const projectId = 'restec-419316';
 
-const uri = "mongodb+srv://shkliarskyiak22:cUxy5UCHUFa682w9@cluster0.jiowjli.mongodb.net/ReservDb?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://shkliarskyiak22:pass@cluster0.jiowjli.mongodb.net/ReservDb?retryWrites=true&w=majority&appName=Cluster0";
 
 let mainMail;
 let mainId;
@@ -41,12 +39,6 @@ async function run() {
 run().catch(console.dir);
 
 
-
-
-
-import readline from 'readline';
-
-
 // Параметри автентифікації
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 const TOKEN_PATH = 'token.json'; // файл для зберігання токенів
@@ -59,10 +51,6 @@ async function authenticate(callback) {
       const credentials = JSON.parse(credentialsData);
 
       const { client_id, client_secret, redirect_uris } = credentials.installed || credentials.web;
-      // Перевіряємо, чи властивості client_id, client_secret та redirect_uris є в об'єкті
-      if (!client_id || !client_secret || !redirect_uris || !redirect_uris[0]) {
-          throw new Error('Missing required properties in credentials.installed');
-      }
 
       const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
@@ -76,43 +64,6 @@ async function authenticate(callback) {
   }
 }
 
-// Отримання нового токену доступу
-async function getAccessToken(oAuth2Client, callback) {
-    const credentialsData = await fs.readFile(CREDENTIALS_PATH);
-    const credentials = JSON.parse(credentialsData);
-
-    const { client_id, client_secret, redirect_uris } = credentials.installed || credentials.web;
-    if (!client_id || !client_secret || !redirect_uris || !redirect_uris[0]) {
-        throw new Error('Missing required properties in credentials.installed');
-    }
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
-        include_granted_scopes: true,
-        client_id: client_id,
-        redirect_uri: redirect_uris
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, async (err, token) => {
-            if (err) return console.error('Error retrieving access token', err);
-            oAuth2Client.setCredentials(token);
-            try {
-                await fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-                console.log('Token stored to', TOKEN_PATH);
-            } catch (err) {
-                console.error(err);
-            }
-            callback(oAuth2Client);
-        });
-    });
-}
-
 // Створення події в Google Календарі
 function createEvent(auth) {
     const calendar = google.calendar({ version: 'v3', auth });
@@ -121,12 +72,12 @@ function createEvent(auth) {
         'summary': 'Test Event',
         'description': 'This is a test event',
         'start': {
-            'dateTime': '2024-04-09T10:00:00',
-            'timeZone': 'Ukraine/Kiev',
+            'dateTime': '2024-04-10T10:00:00',
+            'timeZone': 'America/New_York',
         },
         'end': {
-            'dateTime': '2024-04-09T11:00:00',
-            'timeZone': 'Ukraine/Kiev',
+            'dateTime': '2024-04-10T11:00:00',
+            'timeZone': 'America/New_York',
         },
     };
 
@@ -141,186 +92,6 @@ function createEvent(auth) {
         console.log('Event created:', event.data.htmlLink);
     });
 }
-
-const oauth2Client = new google.auth.OAuth2({
-  client_id : "",
-  client_secret:"",
-  redirect_uris: [
-    "http://localhost:3000/oauth2callback"
-  ]
-});
-
-// Запуск автентифікації та створення події
-// authenticate(createEvent);
-
-
-// const auth = new google.auth.GoogleAuth({
-//   scopes: ['https://www.googleapis.com/auth/calendar/shkliarskyi_ak22@nuwm.edu.ua'],
-// });
-
-// /**
-//  * To use OAuth2 authentication, we need access to a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI
-//  * from the client_secret.json file. To get these credentials for your application, visit
-//  * https://console.cloud.google.com/apis/credentials.
-//  */
-// const oauth2Client = new google.auth.OAuth2({
-//   client_id : "",
-//   client_secret:"",
-//   redirect_uris: [
-//     "http://localhost:8080"
-//   ]
-// });
-
-// // Access scopes for read-only Drive activity.
-// const scopes = [
-//   'https://www.googleapis.com/auth/calendar/shkliarskyi_ak22@nuwm.edu.ua'
-// ];
-
-// // Generate a url that asks permissions for the Drive activity scope
-// const authorizationUrl = oauth2Client.generateAuthUrl({
-//   // 'online' (default) or 'offline' (gets refresh_token)
-//   access_type: 'offline',
-//   /** Pass in the scopes array defined above.
-//     * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
-//   scope: scopes,
-//   // Enable incremental authorization. Recommended as a best practice.
-//   include_granted_scopes: true
-// });
-// console.log(authorizationUrl);
-
-
-
-
-
-
-
-// const SCOPES = ['https://www.googleapis.com/auth/calendar'];
-
-// const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-// const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
-
-// /**
-//  * Reads previously authorized credentials from the save file.
-//  *
-//  * @return {Promise<OAuth2Client|null>}
-//  */
-// async function loadSavedCredentialsIfExist() {
-//   try {
-//     const content = await fs.readFile(TOKEN_PATH);
-//     const credentials = JSON.parse(content);
-//     return google.auth.fromJSON(credentials);
-//   } catch (err) {
-//     return null;
-//   }
-// }
-
-// /**
-//  * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
-//  *
-//  * @param {OAuth2Client} client
-//  * @return {Promise<void>}
-//  */
-// async function saveCredentials(client) {
-//   const content = await fs.readFile(CREDENTIALS_PATH);
-//   const keys = JSON.parse(content);
-//   const key = keys.installed || keys.web;
-//   const payload = JSON.stringify({
-//     type: 'authorized_user',
-//     client_id: key.client_id,
-//     client_secret: key.client_secret,
-//     refresh_token: client.credentials.refresh_token,
-//   });
-//   await fs.writeFile(TOKEN_PATH, payload);
-// }
-
-// /**
-//  * Load or request or authorization to call APIs.
-//  *
-//  */
-// async function authorize() {
-//   let client = await loadSavedCredentialsIfExist();
-//   if (client) {
-//     return client;
-//   }
-//   client = await authenticate({
-//     scopes: SCOPES,
-//     keyfilePath: CREDENTIALS_PATH,
-//   });
-//   if (client.credentials) {
-//     await saveCredentials(client);
-//   }
-//   return client;
-// }
-
-// /**
-//  * Lists the next 10 events on the user's primary calendar.
-//  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
-//  */
-// async function listEvents(auth) {
-//   const calendar = google.calendar({version: 'v3', auth});
-//   const res = await calendar.events.list({
-//     calendarId: 'primary',
-//     timeMin: new Date().toISOString(),
-//     maxResults: 10,
-//     singleEvents: true,
-//     orderBy: 'startTime',
-//   });
-//   const events = res.data.items;
-//   if (!events || events.length === 0) {
-//     console.log('No upcoming events found.');
-//     return;
-//   }
-//   console.log('Upcoming 10 events:');
-//   events.map((event, i) => {
-//     const start = event.start.dateTime || event.start.date;
-//     console.log(`${start} - ${event.summary}`);
-//   });
-// }
-
-// authorize().then(listEvents).catch(console.error);
-
-// async function authenticateImplicitWithAdc() {
-//   // This snippet demonstrates how to list buckets.
-//   // NOTE: Replace the client created below with the client required for your application.
-//   // Note that the credentials are not specified when constructing the client.
-//   // The client library finds your credentials using ADC.
-//   const storage = new Storage({
-//     projectId,
-//   });
-//   const [buckets] = await storage.getBuckets();
-//   console.log('Buckets:');
-
-//   for (const bucket of buckets) {
-//     console.log(`- ${bucket.name}`);
-//   }
-
-//   console.log('Listed all storage buckets.');
-// }
-
-// authenticateImplicitWithAdc();
-
-// async function main() {
-
-//   // Acquire an auth client, and bind it to all future calls
-//   const authClient = await auth.getClient();
-//   google.options({auth: authClient});
-
-//   // Do the magic
-//   const res = await calendar.calendars.clear({
-//     // Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to access the primary calendar of the currently logged in user, use the "primary" keyword.
-//     calendarId: 'shkliarskyi_ak22@nuwm.edu.ua',
-//   });
-//   console.log(res.data);
-// }
-
-// main().catch(e => {
-//   console.error(e);
-//   throw e;
-// });
-
-
-
-
 
 // const event = {
 //   'summary': 'Google I/O 2015',
@@ -350,15 +121,23 @@ const oauth2Client = new google.auth.OAuth2({
 //   },
 // };
 
+const content = await fs.readFile(CREDENTIALS_PATH);
+const credentials = JSON.parse(content);
+const { client_id, client_secret, redirect_uris } = credentials.installed || credentials.web;
 
+  function generateAuthUrl() {
+  console.log(client_id);
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris);
 
+  const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES,
+      client_id: client_id,
+      redirect_uri: redirect_uris
+  });
 
-
-
-
-// AIzaSyB0aZnUboco9gpoYoupsEZCzRCrXAR5R6I  apikey
-
-
+  return authUrl;
+}
 
 async function regster(email, pass, orient){
 
@@ -404,10 +183,23 @@ app.use(express.static("public"))
 app.get("/", (req, res) => {
     res.render("home.ejs")
 })
-app.get("/home.ejs", (req, res) => {
-    res.render("home.ejs");
-    getAccessToken(oauth2Client);
-    // authenticate(createEvent);
+app.get("/home.ejs", async (req, res) => {
+    
+    try {
+      const code = req.query.code;
+      const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris);
+
+      const { tokens } = await oAuth2Client.getToken(code);
+      oAuth2Client.setCredentials(tokens);
+
+      // Save the token to disk
+      await fs.writeFile(TOKEN_PATH, JSON.stringify(tokens));
+
+      res.render("home.ejs");
+  } catch (error) {
+      console.error('Error while handling OAuth2 callback:', error);
+      res.status(500).send('Error while handling OAuth2 callback');
+  }
 
     // calendar.events.insert({
     //   auth: authorizationUrl,
@@ -423,16 +215,15 @@ app.get("/home.ejs", (req, res) => {
 
 })
 app.get("/info.ejs", (req, res) => {
-    res.render("info.ejs")
-    // console.log(authorizationUrl);
-    // res.writeHead(301, { "Location": authorizationUrl })
-    // req.render(authorizationUrl);
+  res.render("info.ejs");
 })
 app.get("/login.ejs", (req, res) => {
     res.render("login.ejs",{wrngMess: ""})
 })
 app.get("/registration.ejs", (req, res) => {
-    res.render("registration.ejs")
+    res.render("registration.ejs");
+    // authenticate(createEvent);
+    console.log("ok");
 })
 
 app.post("/login", async (req, res) => {
@@ -441,7 +232,7 @@ app.post("/login", async (req, res) => {
     const password = req.body.password;
     
     if (await checkInfo(email,password) == true) {
-      // res.writeHead(301, { "Location": authorizationUrl });
+      res.render("login.ejs",{wrngMess: ""})
     } else {
       res.render("login.ejs",{wrngMess: "Wrong email or password"});
     }
@@ -460,7 +251,10 @@ app.get("/student.ejs", (req, res) => {
     res.render("student.ejs", { userMail: bMail})
 })
 app.get("/teacher.ejs", (req, res) => {
-    res.render("teacher.ejs")
+    // res.render("teacher.ejs");
+    const authUrl = generateAuthUrl();
+    console.log(authUrl);
+    res.redirect(authUrl);
 })
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`)
