0dfa81d9f4b2eee1e4d7ead30ab0cdebce69673e
Vlads21ter <shkliarskyi_ak22@nuwm.edu.ua>
2024-05-03 17:08:37 +0300

    Added searching teacher, reworked creating event, added come changes

--- index.js
+++ index.js
@@ -10,16 +10,21 @@ import { v4 as uuidv4 } from "uuid";
 
 const requestId = uuidv4(); // Створення унікального ідентифікатора
 
-const uri = "mongodb+srv://shkliarskyiak22:@cluster0.7wz3h6v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "";
 
 let mainMail;
 let mainId;
 let mainOrin;
 
+let calendarId;
 let nameEv;
 let descriptionEv;
 let startTimeEv; 
 let endTimeEv;
+
+let arraySurname;
+let arrayName;
+let arrayMail;
 
 const ctDate = new Date();
 const timezoneOffsetInMinutes = ctDate.getTimezoneOffset();
@@ -111,7 +116,7 @@ function createEvent(auth) {
     };
 
     calendar.events.insert({
-        calendarId: "primary",
+        calendarId: calendarId,
         resource: event,
     }, (err, event) => {
         if (err) {
@@ -154,7 +159,7 @@ const content = await fs.readFile(CREDENTIALS_PATH);
 const credentials = JSON.parse(content);
 const { client_id, client_secret, redirect_uris } = credentials.installed || credentials.web;
 
-  function generateAuthUrl() {
+function generateAuthUrl() {
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris);
 
   const authUrl = oAuth2Client.generateAuthUrl({
@@ -173,12 +178,18 @@ async function regster(surname, name, nameF, email, pass, orient){
   const user = client.db().collection('user');
 
   await user.insertOne({
+
     surname: surname,
     name: name,
     nameF: nameF,
     email: email,
     pass: pass,
-    orient: orient
+    orient: orient,
+    calendarId: calendarId,
+    cd: client_id,
+    lc: client_secret,
+    di: redirect_uris
+
   });
   
   mainId = await user.findOne({email: email})._id;
@@ -196,12 +207,85 @@ async function checkInfo(email1,pass1){
     mainMail = lg.email;
     mainId = lg._id;
     mainOrin = lg.orient;
+    calendarId = lg.calendarId;
     await client.close();
     return true;
   } else {
     await client.close();
     return false;
   }
+}
+
+async function findTeacher(surname , name){
+
+  await client.connect();
+
+  const user = client.db().collection('user');
+
+  let lg;
+  if (name == "") {
+    lg = await user.find({ surname: { $regex: surname.toString(), $options: 'i' } }).toArray();
+
+    arraySurname = await lg.map(lg => lg.surname);
+    arrayName = await lg.map(lg => lg.name);
+    arrayMail = await lg.map(lg => lg.email);
+  }else{
+    lg = await user.find({ surname: { $regex: surname.toString(), $options: 'i' }, name: { $regex: name.toString(), $options: 'i'} }).toArray();
+    arraySurname = await lg.map(lg => lg.surname);
+    arrayName = await lg.map(lg => lg.name);
+    arrayMail = await lg.map(lg => lg.email);
+  }
+  
+  if (lg != null) {
+    await client.close();
+    return lg;
+  } else {
+    await client.close();
+  }
+}
+
+async function setCalenId(oAuth2Client){
+
+  await client.connect();
+
+  const user = client.db().collection('user');
+
+  const calendar = google.calendar({ version: 'v3', auth: oAuth2Client });
+
+    const newCalendar = await calendar.calendars.insert({
+      requestBody: {
+        summary: 'Час для бронювання',
+        description: ''
+      }
+    });
+
+    calendarId = newCalendar.data.id;
+
+    await user.updateOne(
+      {email: mainMail},
+      {$set:{calendarId: calendarId}}
+    );
+
+    await calendar.acl.insert({
+      calendarId: calendarId,
+      requestBody: {
+        role: 'reader',
+        scope: {
+          type: 'default'
+        }
+      }
+    });  
+    console.log("Calendar was created.");
+    await client.close();
+}
+
+async function getCalenId(mail){
+  await client.connect();
+
+  const user = client.db().collection('user');
+  const lg = await user.findOne({email: mail});
+
+  return lg.calendarId;
 }
 
 const app = express();
@@ -236,7 +320,7 @@ app.post("/login", async (req, res) => {
     }
 
 })
-app.post("/registration", async (req, res) => {
+app.post("/registration", async (req, res) => {  
 
     const surname = req.body.surname;
     const name = req.body.name;
@@ -248,9 +332,10 @@ app.post("/registration", async (req, res) => {
     mainOrin=orient;
 
     regster(surname, name, nameF, email, password, orient);
-    console.log(mainMail);
+    // console.log(mainMail);
     const authUrl = generateAuthUrl();
     res.redirect(authUrl);
+
 })
 app.get("/oAuth.ejs", async (req, res) => {
     
@@ -265,19 +350,62 @@ app.get("/oAuth.ejs", async (req, res) => {
     // Save the token to disk
     await fs.writeFile(TOKEN_PATH, JSON.stringify(tokens));
 
-    res.render("oAuth.ejs", { or: mainOrin});
+    setCalenId(oAuth2Client);
+    //Додати затримку
+    await res.render("oAuth.ejs", { or: mainOrin});
 } catch (error) {
     console.error('Error while handling OAuth2 callback:', error);
     res.status(500).send('Error while handling OAuth2 callback');
 }
 
 })
-app.get("/student.ejs", (req, res) => {
-    var bMail = btoa(mainMail).replace(/=+$/, '');
-    res.render("student.ejs", { userMail: bMail, or: mainOrin});
+
+app.get("/student.ejs", async (req, res) => {
+
+  res.render("student.ejs", {
+    wrngMess: "", 
+    userMail: "", 
+    or: mainOrin,
+    arraySurname: "", 
+    arrayName: "", 
+    arrayMail: ""
+});
+})
+
+
+app.post("/student", async (req, res) => {
+
+  const surname = req.body.vsName;
+  const name = req.body.vName;
+
+  await findTeacher(surname, name);
+
+  res.render("student.ejs", {
+    wrngMess: "", 
+    userMail: "", 
+    or: mainOrin,
+    arraySurname: arraySurname,
+    arrayName: arrayName,
+    arrayMail: arrayMail
+  });
+})
+app.post("/student-tec", async (req, res) => {
+
+  const tecMail = req.body.tecMail;
+
+  var bMail = btoa(await getCalenId(tecMail)).replace(/=+$/, '');
+  res.render("student.ejs", {
+    wrngMess: "", 
+    userMail: bMail, 
+    or: mainOrin,
+    arraySurname: arraySurname,
+    arrayName: arrayName,
+    arrayMail: arrayMail
+  });
 })
 app.get("/teacher.ejs", (req, res) => {
-    res.render("teacher.ejs", {wrngMess: "", or: mainOrin, minTime: currentDate});
+    res.render("teacher.ejs", {wrngMess: "", or: mainOrin, minTime: currentDate
+  });
 })
 app.post("/teacher", async (req, res) => {
 
@@ -286,9 +414,18 @@ app.post("/teacher", async (req, res) => {
   startTimeEv = req.body.evDateSt + ":00"; 
   endTimeEv = req.body.evDateEn + ":00";
 
-  authenticate(createEvent);
+  const stT = new Date(req.body.evDateSt);
+  const enT = new Date(req.body.evDateEn);
 
-  res.render("teacher.ejs",{wrngMess: "", or: mainOrin, minTime: currentDate})
+  if(stT>=enT){
+    res.render("teacher.ejs",{wrngMess: "Кінцевий час мусить бути більший ніж початковий", or: mainOrin, minTime: currentDate})
+  }else{
+    authenticate(createEvent);
+
+    res.render("teacher.ejs",{wrngMess: "", or: mainOrin, minTime: currentDate})
+  }
+
+  
 })
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`)
