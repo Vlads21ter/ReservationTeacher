255448094b1f9922b63cfa24ac4b4e6d3f8751aa
Vlads21ter <shkliarskyi_ak22@nuwm.edu.ua>
2024-05-21 17:00:34 +0300

    Testing railway

--- index.js
+++ index.js
@@ -507,7 +507,7 @@ async function getCalenId(mail){
 }
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"))
